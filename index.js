ea4659af6dab95a032493a74b6918eeba06dfe06
J Quinn <git@jaquinn.com>
2020-10-08 17:20:56 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.once("ready", () => {
+  console.log("Bot Online!");
+});
+
+let deadList = [];
+
+const unmute = msg => {
+  const voiceChannel = msg.member.voice.channel;
+  voiceChannel.members.map(
+    member => !deadList.includes(member) && member.voice.setMute(false)
+  );
+};
+
+client.on("message", msg => {
+  if (msg.content.toLowerCase() === "mute") {
+    console.log("Muting...");
+
+    const voiceChannel = msg.member.voice.channel;
+    voiceChannel.members.map(member => member.voice.setMute(true));
+  }
+
+  if (msg.content.toLowerCase() === "unmute") {
+    console.log("Unmuting...");
+
+    unmute(msg);
+  }
+
+  if (msg.content.toLowerCase() === "dead") {
+    console.log("Dead...");
+
+    msg.member.voice.setMute(true);
+    if (deadList.includes(msg.member)) return;
+    deadList.push(msg.member);
+  }
+
+  if (msg.content.toLowerCase() === "reset") {
+    console.log("Resetting...");
+
+    deadList = [];
+    unmute(msg);
+  }
+});
+
+client.login(process.env.BOT_SECRET);
