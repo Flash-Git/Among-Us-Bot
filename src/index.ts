92b9ba404d242d584975c8929ff31411c1987dc5
J Quinn <git@jaquinn.com>
2020-10-09 12:19:35 +0100

    Improved logging and cleaned code

--- src/index.ts
+++ src/index.ts
@@ -27,34 +27,45 @@ client.once("ready", () => {
 });
 
 client.on("message", (msg: Message) => {
-  if (msg.content.toLowerCase() === "mute") {
-    console.log("Muting...");
+  const message = msg.content.toLowerCase();
 
-    muteAll(msg);
-    msg.delete();
-    return;
+  const date = new Date();
+  const dateMsg = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
+
+  switch (message) {
+    case "mute":
+      console.log(dateMsg + " - Muting...");
+      muteAll(msg);
+      msg.delete();
+      return;
+
+    case "unmute":
+      console.log(dateMsg + " - Unmuting...");
+      unmuteAll(msg);
+      msg.delete();
+      return;
+
+    case "dead":
+      console.log(dateMsg + " - Dead...");
+      msg.member.voice.setMute(true);
+      if (deadList.includes(msg.member)) return;
+      deadList.push(msg.member);
+      msg.delete();
+      return;
+
+    case "reset":
+      console.log(dateMsg + " - Resetting...");
+      deadList.length = 0;
+      unmuteAll(msg);
+      msg.delete();
+      return;
+
+    default:
+      break;
   }
 
-  if (msg.content.toLowerCase() === "unmute") {
-    console.log("Unmuting...");
-
-    unmuteAll(msg);
-    msg.delete();
-    return;
-  }
-
-  if (msg.content.toLowerCase() === "dead") {
-    console.log("Dead...");
-
-    msg.member.voice.setMute(true);
-    if (deadList.includes(msg.member)) return;
-    deadList.push(msg.member);
-    msg.delete();
-    return;
-  }
-
-  if (msg.content.toLowerCase().startsWith("dead ")||msg.content.toLowerCase().startsWith("mute ")) {
-    const name = msg.content.toLowerCase().slice(5);
+  if (message.startsWith("dead ") || message.startsWith("mute ")) {
+    const name = message.slice(5);
     const member = msg.member.voice.channel.members.find(member => member.displayName.toLowerCase() === name);
 
     if (member === undefined) return;
@@ -63,24 +74,15 @@ client.on("message", (msg: Message) => {
 
     if (deadList.includes(member)) return;
 
-    console.log(msg.content);
+    console.log(dateMsg + " - " + msg.content);
 
     deadList.push(member);
     msg.delete();
     return;
   }
-
-  if (msg.content.toLowerCase() === "reset") {
-    console.log("Resetting...");
-
-    deadList.length = 0;
-    unmuteAll(msg);
-    msg.delete();
-    return;
-  }
 });
 
-console.log("Starting...");
+// console.log("Starting...");
 
 client.login(process.env.BOT_SECRET);
 
